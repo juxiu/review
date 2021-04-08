fbfa380a9a94222fff62c5ceb726a43307603b63
juxiu <iguolintao@163.com>
2021-04-08 22:17:03 +0800

    两数相加

---
+++ index.js
@@ -0,0 +1,13 @@
+// var twoSum = function (nums, target) {
+//     let map = new Map();
+//     for (let index = 0; index < nums.length; index++) {
+//         let temp = target - nums[index];
+//         if (map.has(temp)) {
+//             return [map.get(temp), index]
+//         } else {
+//             map.set(nums[index], index);
+//         }
+//     }
+//     return []
+// };
+
